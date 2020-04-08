8170d2fa8374c25a23b21f3d567c3e1f3ac0ba93
Bee <ardelmbiplang@gmail.com>
2020-04-08 13:06:02 +0100

    Optimized to currentlyInfectedAndSevereImpact

--- src/estimator.js
+++ src/estimator.js
@@ -1,29 +1,47 @@
-const inputData = require('./inputData');
-const outputEstimatorData = require('./outputData');
 
 
-const currentlyInfected = (data) => {
-    let outputData = {...outputEstimatorData, impact: { currentlyInfected: data.data.reportedCases * 10}}
-    return outputData
+data = {
+    region: {
+    name: "Africa",
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
 }
 
-const severeImpact = (data) => {
-    let outputData = {...outputEstimatorData, severeImpact: { currentlyInfected: data.data.reportedCases * 50}}
-    return outputData
-}
+
+const currentlyInfectedAndSevereImpact = (data, value) => data.reportedCases * value
+
+
+
 
 const covid19ImpactEstimator = (data) => {
-
-    let severeImpactEstimator = severeImpact(data)
-    let currentlyInfectedEstimator = currentlyInfected(data)
+    const input = data;
+    let currentlyInfectedWithConvid = currentlyInfectedAndSevereImpact(data, 10)
+    let severeImpactWithConvid = currentlyInfectedAndSevereImpact(data, 50)
     
-    return Object.assign(severeImpactEstimator, currentlyInfectedEstimator)
+    return {
+        data: input,
+        impact: { 
+            currentlyInfected: currentlyInfectedWithConvid, 
+            infectionsByRequestedTime: currentlyInfectedWithConvid * 512 
+        },
+        severeImpact: { 
+            currentlyInfected: severeImpactWithConvid, 
+            infectionsByRequestedTime: severeImpactWithConvid * 512 
+        } 
+    }
 
 }
 
 
 // console.log(severeImpact())
-console.log(covid19ImpactEstimator(inputData))
+console.log(covid19ImpactEstimator(data))
 
 module.exports = { 
     covid19ImpactEstimator
