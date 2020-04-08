35ef91fd134b0e4ac3be191a14a4382e5c0bbff5
Bee <ardelmbiplang@gmail.com>
2020-04-08 21:32:07 +0100

    Run Audits

--- src/estimator.js
+++ src/estimator.js
@@ -1,47 +1,44 @@
 
-data = {
-    region: {
-    name: "Africa",
+const inputData = {
+  region: {
+    name: 'Africa',
     avgAge: 19.7,
     avgDailyIncomeInUSD: 5,
     avgDailyIncomePopulation: 0.71
-    },
-    periodType: "days",
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-}
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
 
 
-const currentlyInfectedAndSevereImpact = (inputData, value) => inputData.reportedCases * value
-
-
+const currentlyInfectedAndSevereImpact = (input, value) => input.reportedCases * value;
 
 
 const covid19ImpactEstimator = (data) => {
-    const input = data;
-    let currentlyInfectedWithConvid = currentlyInfectedAndSevereImpact(data, 10)
-    let severeImpactWithConvid = currentlyInfectedAndSevereImpact(data, 50)
-    
-    return {
-        data: input,
-        impact: { 
-            currentlyInfected: currentlyInfectedWithConvid, 
-            infectionsByRequestedTime: currentlyInfectedWithConvid * 512 
-        },
-        severeImpact: { 
-            currentlyInfected: severeImpactWithConvid, 
-            infectionsByRequestedTime: severeImpactWithConvid * 512 
-        } 
-    }
+  const input = data;
+  const currentlyInfectedWithConvid = currentlyInfectedAndSevereImpact(inputData, 10);
+  const severeImpactWithConvid = currentlyInfectedAndSevereImpact(inputData, 50);
 
-}
+  return {
+    data: input,
+    impact: {
+      currentlyInfected: currentlyInfectedWithConvid,
+      infectionsByRequestedTime: currentlyInfectedWithConvid * 512
+    },
+    severeImpact: {
+      currentlyInfected: severeImpactWithConvid,
+      infectionsByRequestedTime: severeImpactWithConvid * 512
+    }
+  };
+};
 
 
 // console.log(severeImpact())
-console.log(covid19ImpactEstimator(data))
+// console.log(covid19ImpactEstimator(inputData))
 
-module.exports = { 
-    covid19ImpactEstimator
+module.exports = {
+  covid19ImpactEstimator
 };
