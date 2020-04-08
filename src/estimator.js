7d44247f331145d7cae4c41c1573a38819184af8
Bee <ardelmbiplang@gmail.com>
2020-04-08 11:36:34 +0100

    computation for both currentlyInfected and severeimpact

--- src/estimator.js
+++ src/estimator.js
@@ -2,16 +2,27 @@ const inputData = require('./inputData');
 const outputEstimatorData = require('./outputData');
 
 
-
-
-const covid19ImpactEstimator = (data) => {
+const currentlyInfected = (data) => {
     let outputData = {...outputEstimatorData, impact: { currentlyInfected: data.data.reportedCases * 10}}
     return outputData
+}
+
+const severeImpact = (data) => {
+    let outputData = {...outputEstimatorData, severeImpact: { currentlyInfected: data.data.reportedCases * 50}}
+    return outputData
+}
+
+const covid19ImpactEstimator = (data) => {
+
+    let severeImpactEstimator = severeImpact(data)
+    let currentlyInfectedEstimator = currentlyInfected(data)
+    
+    return Object.assign(severeImpactEstimator, currentlyInfectedEstimator)
 
 }
 
 
-
+// console.log(severeImpact())
 console.log(covid19ImpactEstimator(inputData))
 
 module.exports = { 
