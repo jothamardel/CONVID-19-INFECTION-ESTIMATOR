3daf2af59f07aead21f571ba103ac48ad4d184a1
Bee <ardelmbiplang@gmail.com>
2020-04-08 11:10:59 +0100

    Setup environment

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,19 @@
-const covid19ImpactEstimator = (data) => data;
+const inputData = require('./inputData');
+const outputEstimatorData = require('./outputData');
 
-export default covid19ImpactEstimator;
+
+
+
+const covid19ImpactEstimator = (data) => {
+    let outputData = {...outputEstimatorData, impact: { currentlyInfected: data.data.reportedCases * 10}}
+    return outputData
+
+}
+
+
+
+console.log(covid19ImpactEstimator(inputData))
+
+module.exports = { 
+    covid19ImpactEstimator
+};
---
+++ src/outputData.js
@@ -0,0 +1,11 @@
+output = {
+    data: {}, // the input data you got
+    impact: {}, // your best case estimation
+    severeImpact: {} // your severe case estimation
+   }
+
+
+
+module.exports = {
+    output
+}
