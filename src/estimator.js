e13cfae4af80052d57604af80367d4e64730ff68
Bee <ardelmbiplang@gmail.com>
2020-04-10 18:53:51 +0100

    Challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,17 @@
 
-const inputData = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'days',
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-};
+// const inputData = {
+//   region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//   },
+//   periodType: 'days',
+//   timeToElapse: 58,
+//   reportedCases: 674,
+//   population: 66622705,
+//   totalHospitalBeds: 1380614
+// };
 
 // const inputData = {
 //   region: {
@@ -50,7 +50,7 @@ const covid19ImpactEstimator = (data) => {
   const casesReportedForImpact = casesReported(reportedCases, 10);
   const casesReportedForSevereImpact = casesReported(reportedCases, 50);
   return {
-    data: data,
+    data,
     imapct: {
       currentlyInfected: casesReportedForImpact,
       infectionsByRequestedTime: casesReportedForImpact
@@ -64,7 +64,7 @@ const covid19ImpactEstimator = (data) => {
   };
 };
 
-covid19ImpactEstimator(inputData);
+// covid19ImpactEstimator(inputData);
 
 // console.log(covid19ImpactEstimator(inputData));
 
